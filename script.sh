7d6fc3caeab42b5458645b764d3c4baa28732e2e
sumanmanna134 <manna.suman134@gmail.com>
2024-01-10 00:44:43 +0530

    add docker-compose config

--- script.sh
+++ script.sh
@@ -19,6 +19,7 @@ install_docker_linux() {
     sudo usermod -a -G docker $(whoami)
     newgrp docker
     echo "Docker installed and started. ✅"
+    sleep 5
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
     sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
     sudo chmod +x /usr/bin/docker-compose
