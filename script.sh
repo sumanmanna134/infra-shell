f21e6ff6e399e63ee308f9d4042d0e4a1fcd8372
sumanmanna134 <manna.suman134@gmail.com>
2024-01-09 19:40:31 +0530

    add options to install and rollout docker

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 install_docker_macos() {
     echo "Installing Docker Desktop for macOS..."
     # Add additional steps if needed for macOS Docker Desktop installation
@@ -15,6 +16,7 @@ install_docker_linux() {
     echo "Docker installed and started."
 }
 
+configure_docker(){
 if command -v docker &> /dev/null; then
     echo "Docker is already installed."
 else
@@ -28,7 +30,35 @@ else
         exit 1
     fi
 fi
+    
+}
+
+echo 
+echo "Welcome 2FA-EXP-ORCHESTRATION 🚀"
+echo "This script allows you to perform various actions to orchestration."
+echo "Please select an option from the menu below:"
+# Menu options
+echo
+options=("Init" "Rollout" "Quit")
+echo
+PS3="> "
+echo
+select option in "${options[@]}"; do
+    case $REPLY in
+        1) configure_docker
+           docker_status=$(docker info &> /dev/null && echo "running" || echo "not running")
+           echo "Docker status: $docker_status"
+            ;;
+        2)  sudo apt-get remove docker.io -y
+            ;;
+        3)
+            echo "Quitting..."
+            break
+            ;;
+        *)
+            echo "Invalid option. Please select a valid option."
+            ;;
+    esac
+done
 
 # Check Docker status
-docker_status=$(docker info &> /dev/null && echo "running" || echo "not running")
-echo "Docker status: $docker_status"
